1bf4327294b91d2aa69dfcdcde8a32d610336127
Andrew Baxter <rhinocratic@googlemail.com>
2018-06-15 19:03:03 +0100

    Changed parameter order

--- src/sorrow/srk.clj
+++ src/sorrow/srk.clj
@@ -32,9 +32,14 @@
             [x' y' r'] (map nxt [x y r])]
         (recur x' y' r')))))
 
+(defn lcm
+  "Calculate the least common multiple of m and n"
+  [m n]
+  (/ (* m n) (last (gcd m n))))
+
 (defn- mod-inverse
   "Find the multiplicative inverse of n mod p, where p is prime"
-  [n p]
+  [p n]
   (-> (gcd n p)
     (first)
     (mod p)))
@@ -50,16 +55,63 @@
   "For a desired encoded word length n and alphabet size p (prime), calculate the
    two weight parameters a, b that will be used in generating weight sequences for
    encoding and decoding.  Returns a vector [a b]"
-  [n p]
+  [p n]
   [(- n 2) (first (filter #(= n (code-length p %)) (range p)))])
 
 (defn- weight-sequences
   "For a desired encoded word length n and alphabet size p (prime), calculate the
    two sequences of weights that will be used in encoding and decoding.  Returns a
    vector of the weight sequences"
-  [n p]
-  (let [[a b] (weight-parameters n p)
+  [p n]
+  (let [[a b] (weight-parameters p n)
         r (range 1 (inc n))
         w (partial + a)
         w' (fn [x] (mod (* (+ a x) (+ b x)) p))]
     [(mapv w r) (mapv w' r)]))
+
+(defn- weighted-sum
+  "Given a sequence of integers is and a sequence of weights ws,
+   return the weighted sum of the integers modulo p."
+  [p is ws]
+  (let [dot-product (apply + (map * is ws))]
+    (mod dot-product p)))
+
+(defn- scalev
+  "Multiply a vector v by the given scale s and reduce the entries modulo p"
+  [p s v]
+  (map #(mod (* s %) p) v))
+
+(defn- minusv
+  "Subract v2 from v1, reducing the entries modulo p"
+  [p v1 v2]
+  (vec (map #(mod (- %1 %2) p) v1 v2)))
+
+(defn- eliminate-ith-term
+  "Eliminate the ith term from the simultaneous congruences modulo p represented by a and b:
+  a0.x0 + a1.x1 + ... + an ≡ 0 (mod p)
+  b0.x0 + b1.x1 + ... + bn ≡ 0 (mod p)"
+  [p a b i]
+  (let [lcm-of-ith-term (lcm (nth a i) (nth b i))
+        scale (fn [v] (scalev p (/ lcm-of-ith-term (nth v i)) v))
+        [sa sb] (map scale [a b])
+        difference (minusv p sa sb)]
+    (concat
+      (subvec difference 0 i)
+      (subvec difference (inc i)))))
+
+(defn- solve-linear-congruence
+  "Find a solution x of the congruence r.x + s ≡ 0 (mod p)"
+  [p r s]
+  (let [t (mod (- p s) p)]
+    (mod (* t (mod-inverse p r)) p)))
+;
+; (defn- solve-simultaneous-congruences
+;   "Solve the following simultaneous congruences for x and y:
+;   a0.x + a1.y + a2 ≡ 0 (mod p)
+;   b0.x + b1.y + b2 ≡ 0 (mod p)"
+;   [a b p]
+;   (let [v1 (eliminate-ith-term a b 0 p)
+;         v2 (eliminate-ith-term a b 1 p)
+;         x (solve-linear-congruence v2 p)
+;         y (solve-linear-congruence v1 p)]
+;     [x y]))
--- test/sorrow/srk_test.clj
+++ test/sorrow/srk_test.clj
@@ -18,9 +18,13 @@
     (is (= [-9 47 2] (#'sorrow.srk/gcd 240 46)))
     (is (= [47 -9 2] (#'sorrow.srk/gcd 46 240)))))
 
+(deftest test-lcm
+  (testing "Least common multiple"
+    (is (= 12 (#'sorrow.srk/lcm 4 6)))))
+
 (deftest test-mod-inverse
   (testing "Modular inverse"
-    (is (= [1 6 4 3 9 2 8 7 5 10] (map #(#'sorrow.srk/mod-inverse % 11) (range 1 11))))))
+    (is (= [1 6 4 3 9 2 8 7 5 10] (map #(#'sorrow.srk/mod-inverse 11 %) (range 1 11))))))
 
 (deftest test-code-length
   (testing "Calculate the length of an encoded word for a given alphabet size and weight parameter"
@@ -28,10 +32,31 @@
 
 (deftest test-weight-parameters
   (testing "Calculation of weight parameters (method 1)"
-    (is (= [11 0] (#'sorrow.srk/weight-parameters 13 37)))))
+    (is (= [11 0] (#'sorrow.srk/weight-parameters 37 13)))))
 
 (deftest test-weight-sequences
   (testing "Calculation of weight sequences (method 1)"
     (is (= [[12 13 14 15 16 17 18 19 20 21 22 23 24]
             [12 26 5 23 6 28 15 4 32 25 20 17 16]]
-           (#'sorrow.srk/weight-sequences 13 37)))))
+           (#'sorrow.srk/weight-sequences 37 13)))))
+
+(deftest test-weighted-sum
+  (testing "Weighted sum of a sequence of integers modulo p"
+    (is (= 17 (#'sorrow.srk/weighted-sum 37 [1 2 3 4 5 6] [1 2 3 4 5 6])))))
+
+(deftest test-scalev
+  (testing "Scale a vector, reducing entries modulo p"
+    (is (= [20 12 21] (#'sorrow.srk/scalev 37 5 [78 32 130])))))
+
+(deftest test-minusv
+  (testing "Subtract vectors, reducing entries modulo p"
+    (is (= [27 2 35] (#'sorrow.srk/minusv 37 [21 8 34] [31 6 36])))))
+
+(deftest test-eliminate-ith-term
+  (testing "Eliminate ith term from a pair of simultaneous congruences modulo p"
+    (is (= [29 33 4] (#'sorrow.srk/eliminate-ith-term 37 [1 2 3 4] [5 6 7 8] 2)))))
+
+(deftest test-solve-linear-congruence
+  (testing "Solve a linear congruence modulo p"
+    (is (= 35 (#'sorrow.srk/solve-linear-congruence 37 26 15)))
+    (is (= 12 (#'sorrow.srk/solve-linear-congruence 37 27 9)))))
